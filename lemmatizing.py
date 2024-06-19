402b5ae46d81684af042653072a48d50157ada78
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-06-20 03:09:14 +0530

    Update lemmatizing.py

--- lemmatizing.py
+++ lemmatizing.py
@@ -1,28 +1,10 @@
 # Lemmatization tries to achieve a similar base “stem” for a word.
 # However, what makes it different is that it finds the dictionary word instead of truncating the original word.
-
-"""from nltk.stem import PorterStemmer
-stemmer = PorterStemmer()
-print(stemmer.stem('studies'))
-print(stemmer.stem('fucking'))
-
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-print(lemmatizer.lemmatize('studies'))
-print(lemmatizer.lemmatize('fucking'))
+import nltk
+nltk.download('wordnet')
 
-# a simpole demonstration of lemmanization
-from nltk import WordNetLemmatizer
-lemma = WordNetLemmatizer()
-word_list = ['study', 'studying', 'studies', 'studied']
-for w in word_list:
-    print(lemma.lemmatize(w))"""
+w1 = WordNetLemmatizer()
+w1.lemmatize('mice')
 
-
-from nltk import WordNetLemmatizer
-leema = WordNetLemmatizer()
-word_list = ['studies', 'leaves', 'decrease', 'plays']
-
-for w in word_list:
-    print(leema.lemmatize(w))
 

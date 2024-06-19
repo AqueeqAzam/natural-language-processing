138f139f49c9e9cac1e9d6e421ee543859da3e32
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-06-20 03:17:15 +0530

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -1,34 +1,15 @@
-# import the python package
-import random
-from nltk import NaiveBayesClassifier
-from nltk.classify import accuracy as nltk_accuracy
-from nltk.corpus import names
+x = "Natural language processing, or NLP, combines computational linguistics—rule-based modeling of human language—with statistical and machine learning models to enable computers and digital devices to recognize, understand and generate text and speech."
 
-def extract_features(word, N=2):
-    last_n_letters = word[-N:]
-    return {'feature': last_n_letters.lower()}
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 
-if __name__=='__main__':
-    male_list = [(name, 'male') for name in names.words ('male.txt')]
-    female_liat = [(name, 'femail') for name in names.words('femail.txt')]
-    data = (male_list + female_list)
+# seperate the word
+w = word_tokenize(x)
+w
 
-random.seed(5)
-random.shuffle(data)
-
-# categories test data
-input_names = ['Aqueeq', 'Ammarah', 'Parvathi', 'prity']
-
-# define the number of sample used for train and test
-num_train = int(0.8 * len(data))
-
-# iterate through different lengths to compare the acuracy
-for i in range(1, 6):
-    print('\n Numbers of end letters:', i)
-    features = [(extract_features(n, i), gender) for (n, gender) in data]
-    train_data, test_data = features[:num_train], features[num_train:]
-    classifier = NaiveBayesClassifier.train(train_data)
-    accuracy = round(100 * nltk_accuracy(classifier, test_data), 2)
-    print('Accuracy =',  + str(accuracy) + '%')
-    for name in input_names:
-        print(name, '==>', classifier.classify(extract_features(name, i)))
+from nltk import pos_tag
+nltk.download('averaged_perceptron_tagger')
+# check part of speech
+p = pos_tag(w)
+p
